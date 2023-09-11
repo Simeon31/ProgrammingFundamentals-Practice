8481a86348fc1c4ab3ae0e0f1e99d3f5f20a27a5
Simeon Markov <simeonmarkov777@gmail.com>
2023-09-11 17:55:25 +0300

    Exercises Part01

---
+++ Codes/Lists/Ex01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Introduction_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("nums[{0}] = {1}", i, list[i]);
+            }
+        }
+    }
+}
---
+++ Codes/Lists/Ex02.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Introduction_Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name = Console.ReadLine().Split().ToList();
+
+            Console.Write(string.Join("; ", name));
+        }
+    }
+}
