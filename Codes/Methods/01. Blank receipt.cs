cc0a647e1a5c897a0fe53303d2081e028439760b
Simeon Markov <simeonmarkov777@gmail.com>
2023-09-15 14:16:54 +0300

    Please enter the commit message for your changes. Lines starting  Exercises with Methods - Part1

---
+++ Codes/Methods/01. Blank receipt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LabEx01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintReceiptHeader();
+            PrintBody();
+            PrintFooter();
+        }
+        private static void PrintReceipt()
+        {
+            PrintReceiptHeader();
+            PrintBody();
+            PrintFooter();
+        }
+        private static void PrintReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+        private static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+        private static void PrintFooter()
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("\u00A9 IT KARIERA");
+        }
+
+
+    }
+}
