3f81fbee4bc5915df69581c90f97096476b0de39
Simeon Markov <simeonmarkov777@gmail.com>
2023-09-17 14:19:59 +0300

    Debugging and fixing code

---
+++ Codes/Debugging/Ex01 - Multiplying even by odd.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMultipleOfOddAndEvenSum(n));
+        }
+        private static int GetSumOfOddDigits(int n)
+        {
+            int sum = 0;
+
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+        private static int GetSumOfEvenDigits(int n)
+        {
+            int sum = 0;
+
+           
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    sum += lastDigit;
+
+                }
+                n /= 10;
+            }
+
+            return sum;
+        }
+        private static int GetMultipleOfOddAndEvenSum(int n)
+        {
+            int evenSum = GetSumOfEvenDigits(n);
+            int oddSum = GetSumOfOddDigits(n);
+            return evenSum * oddSum;
+        }
+    }
+}
---
+++ Codes/Debugging/Ex03 - Price change alert.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class PriceChangeAlert
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double bounds = double.Parse(Console.ReadLine());
+        double lastPrice = double.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            double currenPrice = double.Parse(Console.ReadLine());
+            double diff = CalculatePrice(lastPrice, currenPrice);
+            bool isSignificantDifference = CheckDifference(diff, bounds);
+            string message = GetPrice(currenPrice, lastPrice, diff, isSignificantDifference);
+
+            Console.WriteLine(message);
+
+            lastPrice = currenPrice;
+        }
+    }
+
+    private static string GetPrice(double price, double lastPrice, double difference, bool checks)
+    {
+        string convertion = "";
+        if (difference == 0)
+        {
+            convertion = string.Format("NO CHANGE: {0}", price);
+        }
+        else if (!checks )
+        {
+            convertion = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)", lastPrice, price, difference );
+        }
+        else if (checks && (difference > 0))
+        {
+            convertion = string.Format("PRICE UP: {0} to {1} ({2:F2}%)", lastPrice, price, difference );
+        }
+        else if (checks && (difference < 0))
+        {
+            convertion = string.Format("PRICE DOWN: {0} to {1} ({2:F2}%)", lastPrice, price, difference );
+        }
+        return convertion;
+    }
+    private static bool CheckDifference(double bounds, double checkDifference)
+    {
+        if (bounds >= checkDifference)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private static double CalculatePrice(double previousPrice, double newPrice)
+    {
+        double priceDifference = (newPrice - previousPrice) / previousPrice;
+        return priceDifference;
+    }
+
+}
